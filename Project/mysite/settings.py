f870930c7560905db10b6d984509c625e81bb0a3
DhruvduttRaval <dhruvdutt.raval@gmail.com>
2022-09-13 17:09:36 +0530

    Project

--- Project/mysite/settings.py
+++ Project/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gc_eusrvt8s#ppv)4#ba8+=lt^$=okj57ke3x*2-#pwr@5%-zf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
